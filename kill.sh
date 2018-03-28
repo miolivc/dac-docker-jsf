f50d799c908befdd6d0313a05163249f1a73aaa2
Michelle Oliveira <miolivc@outlook.com>
2018-03-27 22:33:49 -0300

    running on docker

---
+++ kill.sh
@@ -0,0 +1,5 @@
+
+docker kill app database
+docker rm app database
+docker rmi miolivc/app-dac-jsf miolivc/database-dac-jsf
+mvn clean
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 
 docker build -t miolivc/database-dac-jsf . -f Database
-docker run -p 5432:5432 -d --name database miolivc/database-dac-jsf
+
+mvn package
+docker build -t miolivc/app-dac-jsf . -f Application
+
+docker run -p 5433:5432 -d --name database miolivc/database-dac-jsf
+docker run -p 8081:8080 -d --name app --link database:host-database miolivc/app-dac-jsf
